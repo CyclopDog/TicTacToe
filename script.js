3147cb8b3b5af116338e87877a9fd1bf95852d42
erib <eri-b@users.noreply.github.com>
2019-08-26 16:32:30 -0400

    print board, issue with click events

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 const Gameboard = (() => {
-    let board = [];
+    let board = [["X", "O", "O"],["X", "O", "O"],["X", "O", "O"]];
+    let setup = () => {
+      // for (let i = 1; i <= 3; i++){
+      //   for (let j = 1; j <= 3; j++){
+      //     document.querySelector(i + "_" +j).addEventListener('click', event => {
+      //       console.log('click set on ' + i + "_" +j);
+      //     });
+      //   }
+      // }
+      console.log('hello');
+    }
 
-    return board
+    let print = () => {
+      board.forEach( (row, i) => {
+        row.forEach((ele, j) => {
+          let position = document.querySelector(`tr:nth-child(${i + 1}) td:nth-child(${j + 1})`);
+          position.innerHTML = ele;
+        })
+      })
+    }
+    return {board, print}
 })();
 
 const factoryPlayer = (name) => {
-    return { name };
+  let move = (pos) => {
+
+  }
+  return { name };
 };
