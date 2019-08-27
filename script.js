bf39d3e2a266595ece15547caa5e08d4e417360a
erib <eri-b@users.noreply.github.com>
2019-08-27 10:02:42 -0400

    sorting out scope

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+const factoryPlayer = () => {
+  let move = (pos) => {
+
+  }
+  return { name };
+};
+
+
+const Manager = (() => {
+  let player1 = factoryPlayer();
+  let player2 = factoryPlayer();
+
+  let start = () => {
+    document.querySelector("#name-form").addEventListener("submit", function(e) {
+      e.preventDefault();
+      let player1 = namePlayer.p1;
+      let player2 = namePlayer.p2;
+      console.log(player1);
+      document.querySelector("#name-form").style.display = "none";
+      document.querySelector("table").style.display = "block";
+      Gameboard.setup();
+    });
+  }
+
+  //let player1 = factoryPlayer('somethign');
+  return {start}
+})();
+
+
+
 const Gameboard = (() => {
     let board = [["X", "O", "O"],["X", "O", "O"],["X", "O", "O"]];
 
@@ -26,25 +56,21 @@ const Gameboard = (() => {
     return {board, print, setup}
 })();
 
-const factoryPlayer = (name) => {
-  let move = (pos) => {
 
-  }
-  return { name };
-};
 
-document.querySelector("#playerName").addEventListener("submit", function(e) {
-  e.preventDefault();
-  namePlayer();
-  document.querySelector("#playerName").style.display = "none";
-});
 
-const namePlayer = () => {
-  let p1 = document.querySelector("#playerName #p1name").value;
-  let p2 = document.querySelector("#playerName #p2name").value;
-  let p1s = document.querySelector("#playerName #p1symbol").value;
+const namePlayer = (() => {
+  let p1 = document.querySelector("#name-form #p1name").value;
+  let p2 = document.querySelector("#name-form #p2name").value;
+
+  let p1s = document.querySelector("#name-form #p1symbol").value;
   let p2s;
   p1s == "X" ? p2s = "O" : p2s = "X";
-  document.querySelector(".p1").innerHTML = `Player 1: ${p1} - Symbol: ${p1s}`;
-  document.querySelector(".p2").innerHTML = `Player 2: ${p2} - Symbol: ${p2s}`;
-};
+
+  // document.querySelector(".p1").innerHTML = `Player 1: ${p1} - ${p1s}`;
+  // document.querySelector(".p2").innerHTML = `Player 2: ${p2} - ${p2s}`;
+  return {p1, p1s}
+})();
+
+// Start Game
+Manager.start();
