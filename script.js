0d8fb398effcec6fc9843d39d420fbaad8e5e1fd
erib <eri-b@users.noreply.github.com>
2019-08-27 13:41:42 -0400

    refactored start, began turn method

--- script.js
+++ script.js
@@ -8,28 +8,38 @@ const factoryPlayer = (name = "", symbol = "") => {
 const Manager = (() => {
 
   let player1 = factoryPlayer();
+  let player2 = factoryPlayer();
 
   const start = () => {
     document.querySelector("#name-form").addEventListener("submit", function(e) {
       e.preventDefault();
-      let p1 = document.querySelector("#name-form #p1name").value;
-      let p2 = document.querySelector("#name-form #p2name").value;
 
-      let p1s = document.querySelector("#name-form #p1symbol").value;
-      let p2s;
-      p1s == "X" ? p2s = "O" : p2s = "X";
-      document.querySelector("#name-form").style.display = "none";
-      document.querySelector("table").style.display = "block";
+      player1.name = document.querySelector("#name-form #p1name").value;
+      player1.symbol = document.querySelector("#name-form #p1symbol").value;
+
+      player2.name = document.querySelector("#name-form #p2name").value;
+      player2.symbol = player1.symbol == "X" ? "O" : "X";
+
+      Ui.setup(player1, player2);
       Gameboard.setup();
-      player1.name = p1;
-      player1.symbol = p1s;
-
-      console.log(player1);
-
+      turn();
     });
   }
 
-  return {start}
+  const turn = () => {
+    document.querySelector("#turn").innerHTML = Math.random();
+  }
+
+  return {start, turn}
+})();
+
+const Ui = (() => {
+  const setup = (p1,p2) => {
+    document.querySelector("#name-form").style.display = "none";
+    document.querySelector("table").style.display = "block";
+    document.querySelector("#intro").innerHTML = `${p1.name}(${p1.symbol}) vs ${p2.name}(${p2.symbol})`;
+  }
+  return {setup};
 })();
 
 const Gameboard = (() => {
@@ -43,6 +53,7 @@ const Gameboard = (() => {
             // check if empty
             // know which player is making the move
             ele.innerHTML = "move: " + i + " " + j;
+            Manager.turn();
           });
         }
       }
@@ -60,11 +71,6 @@ const Gameboard = (() => {
     return {board, print, setup}
 })();
 
-const namePlayer = (() => {
-  // document.querySelector(".p1").innerHTML = `Player 1: ${p1} - ${p1s}`;
-  // document.querySelector(".p2").innerHTML = `Player 2: ${p2} - ${p2s}`;
-  // return {p1, p2, p1s, p2s}
-})();
 
 // Start Game
 Manager.start();
