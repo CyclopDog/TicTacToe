ebaf4effffa7bd780466637eff3d84174ecdf718
DevMFernandes <contato@marcos-fernandes.com>
2019-08-27 11:50:18 -0300

    player1 setup

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-const factoryPlayer = () => {
+const factoryPlayer = (name = "", symbol = "") => {
   let move = (pos) => {
 
   }
-  return { name };
+  return { name, symbol };
 };
 
-
 const Manager = (() => {
-  let player1 = factoryPlayer();
-  let player2 = factoryPlayer();
 
-  let start = () => {
+  let player1 = factoryPlayer();
+
+  const start = () => {
     document.querySelector("#name-form").addEventListener("submit", function(e) {
       e.preventDefault();
-      let player1 = namePlayer.p1;
-      let player2 = namePlayer.p2;
-      console.log(player1);
+      let p1 = document.querySelector("#name-form #p1name").value;
+      let p2 = document.querySelector("#name-form #p2name").value;
+
+      let p1s = document.querySelector("#name-form #p1symbol").value;
+      let p2s;
+      p1s == "X" ? p2s = "O" : p2s = "X";
       document.querySelector("#name-form").style.display = "none";
       document.querySelector("table").style.display = "block";
       Gameboard.setup();
+      player1.name = p1;
+      player1.symbol = p1s;
+
+      console.log(player1);
+
     });
   }
 
-  //let player1 = factoryPlayer('somethign');
   return {start}
 })();
-
-
 
 const Gameboard = (() => {
     let board = [["X", "O", "O"],["X", "O", "O"],["X", "O", "O"]];
@@ -56,20 +60,10 @@ const Gameboard = (() => {
     return {board, print, setup}
 })();
 
-
-
-
 const namePlayer = (() => {
-  let p1 = document.querySelector("#name-form #p1name").value;
-  let p2 = document.querySelector("#name-form #p2name").value;
-
-  let p1s = document.querySelector("#name-form #p1symbol").value;
-  let p2s;
-  p1s == "X" ? p2s = "O" : p2s = "X";
-
   // document.querySelector(".p1").innerHTML = `Player 1: ${p1} - ${p1s}`;
   // document.querySelector(".p2").innerHTML = `Player 2: ${p2} - ${p2s}`;
-  return {p1, p1s}
+  // return {p1, p2, p1s, p2s}
 })();
 
 // Start Game
