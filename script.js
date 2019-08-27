590be7b062ba191f6db918064243efd8e56ad71b
DevMFernandes <contato@marcos-fernandes.com>
2019-08-27 09:48:58 -0300

    player name and symbol form

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const Gameboard = (() => {
     let board = [["X", "O", "O"],["X", "O", "O"],["X", "O", "O"]];
+
     let setup = () => {
       for (let i = 1; i <= 3; i++){
         for (let j = 1; j <= 3; j++){
           let ele = document.querySelector(`tr:nth-child(${i}) td:nth-child(${j})`);
           ele.addEventListener('click', event => {
-            //check
-            // know which player
+            // check if empty
+            // know which player is making the move
             ele.innerHTML = "move: " + i + " " + j;
           });
         }
       }
-
     }
 
     let print = () => {
@@ -22,6 +22,7 @@ const Gameboard = (() => {
         })
       })
     }
+
     return {board, print, setup}
 })();
 
@@ -30,4 +31,20 @@ const factoryPlayer = (name) => {
 
   }
   return { name };
+};
+
+document.querySelector("#playerName").addEventListener("submit", function(e) {
+  e.preventDefault();
+  namePlayer();
+  document.querySelector("#playerName").style.display = "none";
+});
+
+const namePlayer = () => {
+  let p1 = document.querySelector("#playerName #p1name").value;
+  let p2 = document.querySelector("#playerName #p2name").value;
+  let p1s = document.querySelector("#playerName #p1symbol").value;
+  let p2s;
+  p1s == "X" ? p2s = "O" : p2s = "X";
+  document.querySelector(".p1").innerHTML = `Player 1: ${p1} - Symbol: ${p1s}`;
+  document.querySelector(".p2").innerHTML = `Player 2: ${p2} - Symbol: ${p2s}`;
 };
